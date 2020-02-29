d7e15364e53b74f84024da05e40657eee916512a
Lauro Jr <lauro.juniorspx@gmail.com>
2020-02-28 23:19:39 -0800

    Exercício 16

---
+++ Ex_5_CONVERTER_REAIS_PARA_DÓLAR.py
@@ -0,0 +1,6 @@
+R = (float(input("Insira o valor em Reais para depósito e conversão de sua Viagem: ")))
+D = 4.18
+print("Valor do dólar hoje: $",D)
+print("Depósito confirmado no valor de: R$",R)
+saldo = R / 4.18
+print("Seu saldo para viagens passou de R$",R," (REAIS), para $",saldo," (DÓLARES)")
